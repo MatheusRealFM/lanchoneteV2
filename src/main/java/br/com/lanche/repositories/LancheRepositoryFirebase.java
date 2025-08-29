0b2a5f2b651ea5e3bbdd46ad822bf648582acae9
Matheus Falcão Maciel <matheus05fm@gmail.com>
2025-08-28 22:08:33 -0300

    Implementação de uma função para excluir.

--- src/main/java/br/com/lanche/repositories/LancheRepositoryFirebase.java
+++ src/main/java/br/com/lanche/repositories/LancheRepositoryFirebase.java
@@ -3,29 +3,79 @@ package br.com.lanche.repositories;
 import br.com.lanche.interfaces.LancheRepository;
 import br.com.lanche.models.Lanche;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class LancheRepositoryFirebase implements LancheRepository {
     private List<Lanche> lanches = new ArrayList<>();
 
+    public LancheRepositoryFirebase() {
+    }
+
     public List<Lanche> buscarTodos() {
-        return null;
+        return this.lanches;
     }
 
     public Lanche buscarPorId(int id) {
-        return null;
+        Optional<Lanche> lanche = this.lanches.stream()
+                .filter(l -> l.getId() == id)
+                .findFirst();
+
+        return lanche.orElse(null);
     }
 
     public void adicionar(Lanche lanche) {
-        // Adicionar no firebase
+        this.lanches.add(lanche);
     }
 
     public void excluir(int id) {
-        // Exlcuir no firabase
+        Lanche lancheExcluir = buscarPorId(id);
+
+        if (lancheExcluir != null) {
+            File imagem = new File(lancheExcluir.getCaminhoImagem());
+
+            if (imagem.exists()) {
+                if (imagem.delete()) {
+                    System.out.println("Imagem excluída com sucesso. " + imagem.getName());
+                } else {
+                    System.out.println("Erro ao excluir a imagem. " + imagem.getName());
+                }
+            } else {
+                System.out.println("Erro ao tentar localizar imagem.");
+            }
+
+            this.lanches.remove(lancheExcluir);
+            System.out.println("Lanche com ID " + id + " foi excluído com sucesso.");
+
+        } else {
+            System.out.println("Lanche com ID " + id + " não encontrado.");
+        }
     }
 
-    public void atualizar(int id, Lanche lanche) {
-        // Firebase
+    public void atualizar(int id, Lanche lancheAtualizado) {
+        Lanche lancheExistente = buscarPorId(id);
+
+        if (lancheExistente != null) {
+            if (!lancheExistente.getCaminhoImagem().equals(lancheAtualizado.getCaminhoImagem())) {
+                File imagemAnterior = new File(lancheExistente.getCaminhoImagem());
+                if (imagemAnterior.exists()) {
+                    if (imagemAnterior.delete()) {
+                        System.out.println("Imagem antiga excluída com sucesso.");
+                    } else {
+                        System.out.println("Não foi possível excluir a imagem antiga.");
+                    }
+                }
+            }
+
+            lancheExistente.setNome(lancheAtualizado.getNome());
+            lancheExistente.setPreco(lancheAtualizado.getPreco());
+            lancheExistente.setCaminhoImagem(lancheAtualizado.getCaminhoImagem());
+
+            System.out.println("Lanche com ID " + id + " atualizado com sucesso.");
+        } else {
+            System.out.println("Lanche com ID " + id + " não pôde ser encontrado.");
+        }
     }
 }
